40924cdb0e523d67dbee2a633d1c6030fa60f591
Spenser Jones <spenser.e.jones@gmail.com>
2012-12-21 09:30:18 -0700

    Initial commit - v0.1

---
+++ README.md
@@ -0,0 +1,40 @@
+# Spice (v0.1)
+Developed by MalevolentJalapeno (Spenser Jones)
+
+## Download
+You can download the most recent version of Spice (v0.1) by [clicking here](./spice.js).
+
+## Usage
+Start by initializing Spice() on any entity, array, or even without an argument. After that, start calling some functions on it, and watch in amazement as your script is cut in half (or more!)
+
+## Examples
+### Find and chop the closest living tree
+```javascript
+Spice(Find({ tree: true })).Filter(function(tree) { return tree.durability > 0; }).Closest().Chop();
+```
+### Find an axe in your inventory, and equip it
+```javascript
+Spice(Me).Inventory().GetItemByType('axe').Equip();
+```
+### Find and pick up any objects
+```javascript
+Spice(Find({ isobject: true })).Closest().PickUp();
+```
+
+## Debugging and testing scripts
+From time to time, you may run into a confusing variable or array, and say to yourself "I wish I could see what was in this, without writing for loops and calling Log repeatedly!"
+```javascript
+Spice(GetItemByName('Stone')).InspectProperties();
+```
+
+### Creating objects in the sandbox
+Writing a big script, and tired of collecting resources before starting to test your logic? Sounds like it is time to spawn a few objects at your feet!
+```javascript
+Spice().__CreateStone(5);  // Create 5 stones
+Spice().__CreateBranch(3); // Create 3 branches
+Spice().__CreateWood(1);    // Create 1 wood
+Spice().__CreateObject({ icon: "stoneaxe", isobject: true, name: "Axe", itemtype: "axe" }, 8); // Create 8 defined objects
+```
+
+## Thank You to:
+* skawesome - Assistance with the Class structure logic
---
+++ Spice.js
@@ -0,0 +1,332 @@
+/**
+ * When using Spice, always start with Spice().
+ * This ensures that you're not using stale spices. Blech!
+ * @class
+ * @namespace Spice
+ */
+function Spice(__) { return new SpiceRack(__); }
+
+/**
+ * This is where the kick comes from!
+ * @class
+ * @namespace SpiceRack
+ */
+function SpiceRack(__) {
+  this.version = '0.1';
+  this.__ = this.TopiaObject = __; // This is what we'll call the original object
+  if (typeof this.__ === 'object' && this.__ instanceof Array === false)
+    for (property in this.__)
+      this[property] = this.__[property];
+}
+
+/**
+ * Load an inventory
+ * @param {Object} inventory Can be either an entity (Me), container (Tree), or inventory (Me.inventory)
+ * @return {SpiceRack} Inventory
+ */
+SpiceRack.prototype.Inventory = function Inventory(inventory) {
+  if (typeof inventory === 'undefined')
+    inventory = Me.inventory;
+  else if (typeof inventory.inventory !== 'undefined')
+    inventory = inventory.inventory;
+  return new Spice(inventory);
+};
+
+/**
+ * Find the first item in the chained inventory that is of a specific type
+ * @param {String} itemtype
+ * @return {SpiceRack} Item if found, null if not
+ */
+SpiceRack.prototype.GetItemByType = function GetItemByType(itemtype) {
+  if (typeof itemtype === 'undefined') return this.Abort("Spice.GetItemByType - Please provide an itemtype to search for.");
+  if (typeof this.__ === 'undefined')
+    return new this.Inventory().GetItemByType(itemtype);
+  else if (typeof this.__.inventory !== 'undefined')
+    return new this.Inventory(this.__).GetItemByType(itemtype);
+
+  var inventory = this.__;
+  var items = inventory.length;
+
+  for (var i = 0; i < items; i++) {
+    if (inventory[i].itemtype == itemtype)
+      return new Spice(inventory[i]);
+  }
+  return new Spice(null);
+};
+
+/**
+ * Does the entity's inventory contain items?
+ * @param {Function} [filter] Filter for items of interest
+ */
+SpiceRack.prototype.ContainsItems = function ContainsItems(filter) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.ContainsItems - Please initialize Spice with an entity.");
+
+  // If no filter is specified, allow everything
+  if (typeof filter === 'undefined') filter = function() { return true; };
+
+  return (this.__.inventory && // Has inventory
+          Spice(this.__.inventory).Filter(function(item) { // Filter it's items
+              return item.itemtype === 'wood'; // Does the inventory contain wood?
+            }).__.length > 0); // Does it have at least one item of interest?
+};
+
+/**
+ * Move the chained item
+ * @param {Number} destination 0-based slot number
+ */
+SpiceRack.prototype.MoveItem = function MoveItem(destination) {
+  if (typeof this.__ === 'undefined') return new Spice();
+
+  UseAction("MoveItem", { target: this.__.slot, destination: destination });
+};
+
+/**
+ * Equip the chained item
+ * @param {String} destination Accessory, Back, Body, or Held.
+ */
+SpiceRack.prototype.Equip = function Equip(destination) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Equip - Please initialize Spice with an item to equip.");
+
+  var named_slots = { 'accessory': 30, 'back': 31, 'body': 32, 'held': 33 };
+  if (typeof destination === 'undefined')
+    destination = 33;
+  else if (named_slots[destination.toLowerCase()])
+    destination = named_slots[destination.toLowerCase()];
+  this.MoveItem(destination);
+};
+
+/**
+ * Move towards and pick up an object
+ */
+SpiceRack.prototype.PickUp = function PickUp() {
+  if (typeof this.__ === 'undefined') return new Spice();
+
+  if (IsAdjacent(Me, this.__) === true)
+    UseAction('Get', this.__);
+  else
+    MoveTowards(this.__);
+};
+
+/**
+ * Drop the item (and move to the coordinates first, if necessary)
+ * @param {Object} coordinates The coordinates to drop the item at
+ */
+SpiceRack.prototype.Drop = function Drop(coordinates) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Drop - Please initialize Spice with an item to drop.");
+
+  if (!coordinates || typeof coordinates.x === 'undefined' || typeof coordinates.y === 'undefined')
+    coordinates = { x: Me.x, y: Me.y };
+
+  if (this.__IsNextTo(coordinates, Me) === true) {
+    UseAction("Drop", { slot: this.__.slot, destination: coordinates });
+    return true;
+  } else {
+    MoveTowards(coordinates);
+    return false;
+  }
+};
+
+/**
+ * Loot the entity
+ * @param {Function} [filter] Only look items that match this filter
+ */
+SpiceRack.prototype.Loot = function Loot(filter) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Loot - Please initialize Spice with an entity to loot.");
+
+  if (typeof filter === 'function' && this.ContainsItems(filter) === false)
+    return false;
+
+  if (this.__IsNextTo(Me) === false)
+    MoveTowards(this.__);
+  else
+    UseAction("Get", { id: this.__.id, slot: _.filter(this.__.inventory, filter)[0].slot });
+  return true;
+};
+
+/**
+ * Chop the entity (and move to the coordinates first, if necessary)
+ * @param {Object} coordinates The coordinates of the entity to chop
+ */
+SpiceRack.prototype.Chop = function Chop() {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Chop - Please initialize Spice with an entity to chop.");
+
+  if (IsAdjacent(this.__, Me) === true) {
+    UseAction("Chop", this.__);
+    return true;
+  } else {
+    MoveTowards(this.__);
+    return false;
+  }
+};
+
+/**
+ * Find conditions within a certain distance
+ * @param {Number} [distance] How far do you want to search?
+ * @param {Object} [target] Which target are we searching around? Defaults to Me
+ */
+SpiceRack.prototype.FindWithinDistance = function FindWithinDistance(distance, target) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.FindWithinDistance - Please initialize Spice with conditions for a Find(), or with {}.");
+  
+  if (typeof distance === 'undefined') distance = 10;
+  if (typeof target   === 'undefined') target   = Me;
+
+  var conditions = this.__;
+  conditions.location = { "$within": { "$center": [[ target.x, target.y ], distance * 32 ]}};
+  return new Spice(Find(conditions));
+};
+
+/**
+ * Filter an array of entities
+ * @param {Function} filter Function that returns true if the entity should be kept, and false if it should be filtered out
+ */
+SpiceRack.prototype.Filter = function Filter(filter) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Filter - Please initialize Spice with an array to filter.");
+  if (typeof filter  === 'undefined') return this.Abort("Spice.Filter - Please provide a function to filter by.");
+
+  return new Spice(_.filter(this.__, filter));
+};
+
+/**
+ * Find the closest entity of an array
+ * @param {Object} target What are we checking if this object is closest to?
+ */
+SpiceRack.prototype.Closest = function Closest(target) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Closest - Please initialize Spice with an array of entities.");
+
+  if (typeof target === 'undefined') target = Me;
+
+  // Set up the variables
+  var matches = this.__, best_match = null, current_distance = 9999;
+
+  // Loop through all of the matches, looking for the best
+  for (var i = 0; i < matches.length; i++) {
+    var examining = matches[i];
+
+    // If this match is closer than our currently selected best_match, use this one instead
+    if (Distance(examining, target) < current_distance) {
+      best_match = examining;
+      current_distance = Distance(examining, target);
+    }
+  }
+  // If we've reached this point, we've checked one or more matches and determined that this is the best one
+  return new Spice(best_match);
+};
+
+/**
+ * Stop the script, log an error, and say the error
+ * @param {String} message Reason for aborting the script
+ */
+SpiceRack.prototype.Abort = function Abort(message) {
+  Stop();
+  Log(message);
+  UseAction("Say", message);
+};
+
+/**
+ * Log the properties and values of an object
+ * @param {Boolean} recursive Do you want to inspect the object recursively? [BROKEN]
+ */
+SpiceRack.prototype.InspectProperties = function InspectProperties(recursive) {
+  if (typeof this.__ === 'undefined') return new Spice();
+
+  var output = "\n";
+  if (this.__ instanceof Array) {
+    output += "Array of " + this.__.length + "\n";
+    for (var i = 0; i < this.__.length; i++)
+      output += this.__[i].toString() + "\n";
+  } else if (typeof this.__ === 'object') {
+    for (var property in this.__)
+      output += property + ": " + this.__[property] + "\n";
+  } else
+    output += this.__.toString();
+  Log(output);
+};
+
+
+
+/**
+ * The following functions should only be used in a sandbox!
+ */
+
+
+
+/**
+ * Run a benchmark on the chained function
+ * @param {Number} times How many times should we run this function?
+ * @return {Number} Benchmark time in milliseconds
+ */
+SpiceRack.prototype.Benchmark = function Benchmark(times) {
+  if (typeof this.__ === 'undefined') return this.Abort("Spice.Benchmark - Please initialize Spice with a function to benchmark.");
+
+  var benchmark_time1 = new Date();
+  for (var i = 0; i < times; i++)
+    this.__();
+  var benchmark_time2 = new Date();
+  var total = (benchmark_time2.getTime() - benchmark_time1.getTime());
+  return total;
+};
+
+/**
+ * Create an object at your feet
+ * @param {Object} entity Entity configuration objects
+ * @param {Number} [amount] How many should be created? Defaults to 1
+ */
+SpiceRack.prototype.__CreateObject = function __CreateObject(entity, amount) {
+  if (typeof entity.x === 'undefined') entity.x = Me.x;
+  if (typeof entity.y === 'undefined') entity.y = Me.y;
+  if (typeof amount === 'undefined')    amount  = 1;
+  
+  var entities = [];
+  for (var i = 0; i < amount; i++)
+    entities.add(World.Create(World.DeepClone(entity)));
+  return entities;
+};
+
+/**
+ * Create an stone at your feet
+ * @param {Number} amount How many should be created?
+ */
+SpiceRack.prototype.__CreateStone = function __CreateStone(amount) {
+  return Spice.Sandbox.CreateObject({ icon: "SmallRock", isobject: true, name: "Stone", itemtype: "stone" }, amount);
+};
+
+/**
+ * Create an branch at your feet
+ * @param {Number} amount How many should be created?
+ */
+SpiceRack.prototype.__CreateBranch = function CreateBranch(amount) {
+  return Spice.Sandbox.CreateObject({ icon: "Branch", isobject: true, name: "Branch", itemtype: "branch" }, amount);
+};
+
+/**
+ * Create some wood at your feet
+ * @param {Number} amount How many should be created?
+ */
+SpiceRack.prototype.__CreateWood = function __CreateWood(amount) {
+  return Spice.Sandbox.CreateObject({ icon: "log", isobject: true, name: "Wood", itemtype: "wood" }, amount);
+};
+
+
+
+/**
+ * Waiting for these functions to be added to the Topia scripting engine
+ */
+
+
+
+/**
+ * Is the selected object next to the target
+ * @param {Object} target
+ * @param {Object} [target2] You can specify an additional target directly, or use the already-loaded entity in this.__
+ */
+SpiceRack.prototype.__IsNextTo = function __IsNextTo(target, target2) {
+  if (typeof target2 === 'undefined') {
+    if (typeof this.__ === 'undefined') return this.Abort("Spice.__IsNextTo - You're missing an argument.");
+    else target2 = this.__;
+  }
+
+  var xDistance = Math.abs(target.x - target2.x);
+  var yDistance = Math.abs(target.y - target2.y);
+
+  return (xDistance < 64 && yDistance < 64);
+};
