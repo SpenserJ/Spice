52b4a174eab038e6b110da833b018796e4899c2a
Spenser Jones <spenser.e.jones@gmail.com>
2012-12-26 20:16:59 -0700

    Added A* Pathfinding
    https://github.com/bgrins/javascript-astar

--- Spice.js
+++ Spice.js
@@ -25,6 +25,16 @@ function SpiceRack(__) {
 }
 
 /**
+ * An enum for directions
+ */
+SpiceRack.prototype.DirectionEnum = {
+  North: 0, 0: 'North',
+  East:  1, 1: 'East',
+  South: 2, 2: 'South',
+  West:  3, 3: 'West'
+}
+
+/**
  * Load an inventory
  * @param {Object} inventory Can be either an entity (Me), container (Tree),
  *     or inventory (Me.inventory)
@@ -336,25 +346,25 @@ SpiceRack.prototype.ClosestPoint = function ClosestPoint(target) {
 
 /**
  * Calculate the distance to a 1x1 object, or a multidimensional object
- * @oaram {Object} target What are we checking the distance to?
- * @param {Object} [target2] You can specify an additional target directly, or
+ * @oaram {Object} from What are we checking the distance from?
+ * @param {Object} [to] You can specify an additional target directly, or
  *     use the already-loaded entity in this.__
  * @return {Number} Distance (in pixels) between this target and the selection.
  */
-SpiceRack.prototype.Distance = function Distance(target, target2) {
-  if (typeof target2 === 'undefined') {
+SpiceRack.prototype.Distance = function Distance(from, to) {
+  if (typeof to === 'undefined') {
     if (typeof this.__ === 'undefined')
       return this.Abort("Spice.Distance - You're missing an argument.");
-    target2 = this;
-  } else if (typeof target2.__ === 'undefined') {
-    target2 = Spice(target2);
+    to = this;
+  } else if (typeof to.__ === 'undefined') {
+    to = Spice(to);
   }
-  if (typeof target.__ === 'undefined') target = Spice(target);
+  if (typeof from.__ === 'undefined') from = Spice(from);
 
-  if (target.IsMultiDimensional() === true)
-    target = target.ClosestPoint(target2);
-  if (target2.IsMultiDimensional() === true)
-    target2 = target2.ClosestPoint(target);
+  if (from.IsMultiDimensional() === true)
+    from = target.ClosestPoint(to);
+  if (to.IsMultiDimensional() === true)
+    to = to.ClosestPoint(from);
 
   // We now have the two points that are closest to each other, and can
   // calculate the distance between them.
@@ -368,6 +378,29 @@ SpiceRack.prototype.Distance = function Distance(target, target2) {
 
   return totalDistance;
 }
+
+/**
+ * Determine the direction of the target
+ * @oaram {Object} from What are we checking the direction from?
+ * @param {Object} [to] You can specify an additional target directly, or
+ *     use the already-loaded entity in this.__
+ */
+SpiceRack.prototype.Direction = function Direction(from, to) {
+  if (typeof target2 === 'undefined') {
+    if (typeof this.__ === 'undefined')
+      return this.Abort("Spice.Distance - You're missing an argument.");
+    to = this;
+  } else if (typeof to.__ === 'undefined') {
+    to = Spice(to);
+  }
+  if (typeof from.__ === 'undefined') from = Spice(from);
+
+  if (to.x < from.x) return this.DirectionEnum.West;
+  if (to.x > from.x) return this.DirectionEnum.East;
+  if (to.y < from.y) return this.DirectionEnum.North;
+  if (to.y > from.y) return this.DirectionEnum.South;
+}
+
 
 /**
  * Is the object or entity multi-dimensional?
@@ -437,6 +470,132 @@ SpiceRack.prototype.IsNextTo = function IsNextTo(target, target2) {
 
   return (xDistance < 64 && yDistance < 64);
 };
+
+/**
+ * Move toward a certain location or path
+ */
+SpiceRack.prototype.MoveTowards = function MoveTowards() {
+  if (typeof this.__ === 'undefined')
+    return this.Abort("Spice.MoveTowards - Please initialize Spice with an " +
+        "object, entity, or path array.");
+  var to = this.__;
+  if (to instanceof Array) to = to[0];
+    
+  var totalDistance = Math.abs(Me.x - to.x) + Math.abs(Me.y - to.y);
+  // If we're not right beside it, generate a path and start walking down it.
+  if (totalDistance > 32)
+    return Spice(this.FindPath(Me, true)).MoveTowards();
+
+  UseAction("Move", Spice(to).Direction(Me));
+}
+
+/**
+ * A* pathfinding algorithm that accepts maps with and without tile weighting
+ * @param {Object} from
+ * @param {Object} [to] You can specify a target directly, or use the 
+ *     already-loaded entity in this.__
+ * @param {Boolean} [visible] Whether or not to pathfind on just the visible
+ *     portion of the map. Defaults to true
+ */
+SpiceRack.prototype.FindPath = function FindPath(from, to, visible) {
+  if (typeof to === 'boolean') {
+    visible = to;
+    to = undefined;
+  } else if (typeof to === 'undefined') {
+    if (typeof this.__ === 'undefined')
+      return this.Abort("Spice.IsNextTo - You're missing an argument.");
+    to = this.__;
+  }
+  if (typeof visible === 'undefined') visible = true;
+
+  var map_data = this.GenerateMap(visible);
+  // Set our target as "walkable", otherwise a path can't be generated
+  map_data.map[(to.y   - map_data.y) / globalTileDistance - 1]
+              [(to.x   - map_data.x) / globalTileDistance - 1] = 1;
+  var graph = new Graph(Spice(map_data.map).FlipArray());
+  //var graph = new Graph(map_data.map);
+  var start = graph.nodes[(from.x - map_data.x) / globalTileDistance - 1]
+                         [(from.y - map_data.y) / globalTileDistance - 1];
+  var end   = graph.nodes[(to.x   - map_data.x) / globalTileDistance - 1]
+                         [(to.y   - map_data.y) / globalTileDistance - 1];
+  var path  = astar.search(graph.nodes, start, end);
+
+  var cleaned_path = [];
+  for (var i = 0; i < path.length; i++) {
+    cleaned_path.push({
+      x: ((path[i].x + 1) * globalTileDistance) + map_data.x,
+      y: ((path[i].y + 1) * globalTileDistance) + map_data.y
+    });
+  }
+
+  return cleaned_path;
+}
+
+/**
+ * Generate a weighted map of the world for pathfinding
+ * @param {Boolean} [visible] Whether or not to generate a map of just the
+ *     visible portion of the map. Defaults to true
+ */
+SpiceRack.prototype.GenerateMap = function GenerateMap(visible) {
+  if (typeof visible === 'undefined') visible = true;
+  var roads = Spice(Find({ name: "Dirt Road" }));
+  var obstacles = Spice(Find({ obstacle: true }));
+  if (visible === true) {
+    roads = roads.__Visible();
+    obstacles = obstacles.__Visible();
+  }
+
+  var map_data = {
+    x: Me.x - 11 * globalTileDistance,
+    y: Me.y - 8 * globalTileDistance,
+    w: 21 * globalTileDistance,
+    h: 15 * globalTileDistance,
+    map: []
+  };
+  for (var y  = map_data.y;
+           y  < map_data.y + map_data.h;
+           y += globalTileDistance) {
+    var map_row = [];
+    for (var x  = map_data.x;
+             x  < map_data.x + map_data.w;
+             x += globalTileDistance) {
+      map_row.push(1);
+    }
+    map_data.map.push(map_row);
+  }
+
+  for (var i = 0; i < roads.__.length; i++) {
+    map_data.map[(roads.__[i].y - map_data.y) / globalTileDistance]
+                [(roads.__[i].x - map_data.x) / globalTileDistance] = 0.5; // Faster
+  }
+  for (var i = 0; i < obstacles.__.length; i++) {
+    map_data.map[(obstacles.__[i].y - map_data.y) / globalTileDistance - 1]
+                [(obstacles.__[i].x - map_data.x) / globalTileDistance - 1] = 0; // Blocked
+  }
+
+  map_data.map[(Me.y - map_data.y) / globalTileDistance - 1]
+              [(Me.x - map_data.x) / globalTileDistance - 1] = 5;
+
+  return map_data;
+}
+
+/**
+ * Flip the axis of a 2d array
+ */
+SpiceRack.prototype.FlipArray = function FlipArray() {
+  if (typeof this.__ === 'undefined')
+    return this.Abort("Spice.FlipArray - Please initialize Spice with an " +
+        "array to flip.");
+  var new_array = [];
+  for (var y = 0; y < this.__.length; y++) {
+    for (var x = 0; x < this.__[y].length; x++) {
+      if (typeof new_array[x] === 'undefined')
+        new_array[x] = [];
+      new_array[x][y] = this.__[y][x];
+    }
+  }
+  return new_array;
+}
 
 /**
  * Stop the script, log an error, and say the error
@@ -591,7 +750,7 @@ SpiceRack.prototype.__Delete = function __Delete(deleting) {
  * @param {Number} [slot] The slot to insert the item into
  */
 SpiceRack.prototype.__MoveToInventory = function __MoveToInventory(slot) {
-  if (this.__ instanceof Array) {
+  if (this.__ instanceof Array === true) {
     for (var i = 0; i < this.__.length; i++)
       Spice(this.__[i]).__MoveToInventory(slot + i);
     return;
@@ -614,7 +773,7 @@ SpiceRack.prototype.__MoveToInventory = function __MoveToInventory(slot) {
  * Remove an item from your inventory without UseAction("Drop")
  */
 SpiceRack.prototype.__RemoveFromInventory = function __RemoveFromInventory() {
-  if (this.__ instanceof Array) {
+  if (this.__ instanceof Array === true) {
     for (var i = 0; i < this.__.length; i++)
       Spice(this.__[i]).__RemoveFromInventory();
     return;
@@ -629,3 +788,310 @@ SpiceRack.prototype.__RemoveFromInventory = function __RemoveFromInventory() {
   World.Save(Me);
   usingAction = false;
 }
+
+/**
+ * Filter an array for objects that are out of our field of vision
+ */
+SpiceRack.prototype.__Visible = function __Visible() {
+  if (this.__ instanceof Array === false)
+    return this.Abort("Spice.__Visible - Please initialize Spice " +
+        "with an array of coordinates (or objects with) to filter.");
+  return this.Filter(function(checking) {
+    return (checking.x >= Me.x - 10 * globalTileDistance &&
+            checking.x <= Me.x + 10 * globalTileDistance &&
+            checking.y >= Me.y - 7 * globalTileDistance  &&
+            checking.y <= Me.y + 7 * globalTileDistance);
+  });
+}
+
+
+
+/**
+ * These functions should be included when support for that is added
+ */
+// javascript-astar
+// http://github.com/bgrins/javascript-astar
+// Freely distributable under the MIT License.
+// Implements the astar search algorithm in javascript using a binary heap.
+// Includes Binary Heap (with modifications) from Marijn Haverbeke.
+// http://eloquentjavascript.net/appendix2.html
+var astar = {
+  init: function(grid) {
+    for(var x = 0, xl = grid.length; x < xl; x++) {
+      for(var y = 0, yl = grid[x].length; y < yl; y++) {
+        var node = grid[x][y];
+        node.f = 0;
+        node.g = 0;
+        node.h = 0;
+        node.cost = node.type;
+        node.visited = false;
+        node.closed = false;
+        node.parent = null;
+      }
+    }
+  },
+  heap: function() {
+    return new BinaryHeap(function(node) { 
+      return node.f; 
+    });
+  },
+  search: function(grid, start, end, diagonal, heuristic) {
+    astar.init(grid);
+    heuristic = heuristic || astar.manhattan;
+    diagonal = !!diagonal;
+    var openHeap = astar.heap();
+    openHeap.push(start);
+    while(openHeap.size() > 0) {
+      // Grab the lowest f(x) to process next.  Heap keeps this sorted for us.
+      var currentNode = openHeap.pop();
+      // End case -- result has been found, return the traced path.
+      if(currentNode === end) {
+        var curr = currentNode;
+        var ret = [];
+        while(curr.parent) {
+          ret.push(curr);
+          curr = curr.parent;
+        }
+        return ret.reverse();
+      }
+      // Normal case -- move currentNode from open to closed, process each of its neighbors.
+      currentNode.closed = true;
+      // Find all neighbors for the current node. Optionally find diagonal neighbors as well (false by default).
+      var neighbors = astar.neighbors(grid, currentNode, diagonal);
+      for(var i=0, il = neighbors.length; i < il; i++) {
+        var neighbor = neighbors[i];
+        if(neighbor.closed || neighbor.isWall()) {
+          // Not a valid node to process, skip to next neighbor.
+          continue;
+        }
+        // The g score is the shortest distance from start to current node.
+        // We need to check if the path we have arrived at this neighbor is the shortest one we have seen yet.
+        var gScore = currentNode.g + neighbor.cost;
+        var beenVisited = neighbor.visited;
+        if(!beenVisited || gScore < neighbor.g) {
+          // Found an optimal (so far) path to this node.  Take score for node to see how good it is.
+          neighbor.visited = true;
+          neighbor.parent = currentNode;
+          neighbor.h = neighbor.h || heuristic(neighbor.pos, end.pos);
+          neighbor.g = gScore;
+          neighbor.f = neighbor.g + neighbor.h;
+          if (!beenVisited) {
+            // Pushing to heap will put it in proper place based on the 'f' value.
+            openHeap.push(neighbor);
+          }
+          else {
+            // Already seen the node, but since it has been rescored we need to reorder it in the heap
+            openHeap.rescoreElement(neighbor);
+          }
+        }
+      }
+    }
+    // No result was found - empty array signifies failure to find path.
+    return [];
+  },
+  manhattan: function(pos0, pos1) {
+    // See list of heuristics: http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+    var d1 = Math.abs (pos1.x - pos0.x);
+    var d2 = Math.abs (pos1.y - pos0.y);
+    return d1 + d2;
+  },
+  neighbors: function(grid, node, diagonals) {
+    var ret = [];
+    var x = node.x;
+    var y = node.y;
+    // West
+    if(grid[x-1] && grid[x-1][y]) {
+      ret.push(grid[x-1][y]);
+    }
+    // East
+    if(grid[x+1] && grid[x+1][y]) {
+      ret.push(grid[x+1][y]);
+    }
+    // South
+    if(grid[x] && grid[x][y-1]) {
+      ret.push(grid[x][y-1]);
+    }
+    // North
+    if(grid[x] && grid[x][y+1]) {
+      ret.push(grid[x][y+1]);
+    }
+    if (diagonals) {
+      // Southwest
+      if(grid[x-1] && grid[x-1][y-1]) {
+        ret.push(grid[x-1][y-1]);
+      }
+      // Southeast
+      if(grid[x+1] && grid[x+1][y-1]) {
+        ret.push(grid[x+1][y-1]);
+      }
+      // Northwest
+      if(grid[x-1] && grid[x-1][y+1]) {
+        ret.push(grid[x-1][y+1]);
+      }
+      // Northeast
+      if(grid[x+1] && grid[x+1][y+1]) {
+        ret.push(grid[x+1][y+1]);
+      }
+    }
+    return ret;
+  }
+};
+
+var GraphNodeType = { 
+  OPEN: 1, 
+  WALL: 0 
+};
+// Creates a Graph class used in the astar search algorithm.
+function Graph(grid) {
+  var nodes = [];
+  for (var x = 0; x < grid.length; x++) {
+    nodes[x] = [];
+    
+    for (var y = 0, row = grid[x]; y < row.length; y++) {
+      nodes[x][y] = new GraphNode(x, y, row[y]);
+    }
+  }
+  this.input = grid;
+  this.nodes = nodes;
+}
+Graph.prototype.toString = function() {
+  var graphString = "\n";
+  var nodes = this.nodes;
+  var rowDebug, row, y, l;
+  for (var x = 0, len = nodes.length; x < len; x++) {
+    rowDebug = "";
+    row = nodes[x];
+    for (y = 0, l = row.length; y < l; y++) {
+      rowDebug += row[y].type + " ";
+    }
+    graphString = graphString + rowDebug + "\n";
+  }
+  return graphString;
+};
+function GraphNode(x,y,type) {
+  this.data = { };
+  this.x = x;
+  this.y = y;
+  this.pos = {
+    x: x, 
+    y: y
+  };
+  this.type = type;
+}
+GraphNode.prototype.toString = function() {
+  return "[" + this.x + " " + this.y + "]";
+};
+GraphNode.prototype.isWall = function() {
+  return this.type == GraphNodeType.WALL;
+};
+
+function BinaryHeap(scoreFunction){
+  this.content = [];
+  this.scoreFunction = scoreFunction;
+}
+BinaryHeap.prototype = {
+  push: function(element) {
+    // Add the new element to the end of the array.
+    this.content.push(element);
+    // Allow it to sink down.
+    this.sinkDown(this.content.length - 1);
+  },
+  pop: function() {
+    // Store the first element so we can return it later.
+    var result = this.content[0];
+    // Get the element at the end of the array.
+    var end = this.content.pop();
+    // If there are any elements left, put the end element at the
+    // start, and let it bubble up.
+    if (this.content.length > 0) {
+       this.content[0] = end;
+       this.bubbleUp(0);
+    }
+    return result;
+  },
+  remove: function(node) {
+    var i = this.content.indexOf(node);
+  
+    // When it is found, the process seen in 'pop' is repeated
+    // to fill up the hole.
+    var end = this.content.pop();
+    if (i !== this.content.length - 1) {
+      this.content[i] = end;
+      
+      if (this.scoreFunction(end) < this.scoreFunction(node)) {
+        this.sinkDown(i);
+      }
+      else {
+        this.bubbleUp(i);
+      }
+    }
+  },
+  size: function() {
+    return this.content.length;
+  },
+  rescoreElement: function(node) {
+    this.sinkDown(this.content.indexOf(node));
+  },
+  sinkDown: function(n) {
+    // Fetch the element that has to be sunk.
+    var element = this.content[n];
+    // When at 0, an element can not sink any further.
+    while (n > 0) {
+      // Compute the parent element's index, and fetch it.
+      var parentN = ((n + 1) >> 1) - 1,
+        parent = this.content[parentN];
+      // Swap the elements if the parent is greater.
+      if (this.scoreFunction(element) < this.scoreFunction(parent)) {
+        this.content[parentN] = element;
+        this.content[n] = parent;
+        // Update 'n' to continue at the new position.
+        n = parentN;
+      }
+      // Found a parent that is less, no need to sink any further.
+      else {
+        break;
+      }
+    }
+  },
+  bubbleUp: function(n) {
+    // Look up the target element and its score.
+    var length = this.content.length,
+      element = this.content[n],
+      elemScore = this.scoreFunction(element);
+    
+    while(true) {
+      // Compute the indices of the child elements.
+      var child2N = (n + 1) << 1, child1N = child2N - 1;
+      // This is used to store the new position of the element,
+      // if any.
+      var swap = null;
+      // If the first child exists (is inside the array)...
+      if (child1N < length) {
+      // Look it up and compute its score.
+      var child1 = this.content[child1N],
+        child1Score = this.scoreFunction(child1);
+      // If the score is less than our element's, we need to swap.
+      if (child1Score < elemScore)
+        swap = child1N;
+      }
+      // Do the same checks for the other child.
+      if (child2N < length) {
+        var child2 = this.content[child2N],
+          child2Score = this.scoreFunction(child2);
+        if (child2Score < (swap === null ? elemScore : child1Score)) {
+          swap = child2N;
+        }
+      }
+      // If the element needs to be moved, swap it, and continue.
+      if (swap !== null) {
+        this.content[n] = this.content[swap];
+        this.content[swap] = element;
+        n = swap;
+      }
+      // Otherwise, we are done.
+      else {
+        break;
+      }
+    }
+  }
+};
